305a304704fef8e49e9d44f8eb8733006d58150d
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-10 22:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,6 @@ let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random(0, 20);
 
 let randomInteger = Math.floor(randomNumber);
+
+let randomUserID = randomInteger + 1000000000; 
+
